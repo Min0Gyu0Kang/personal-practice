7f2ad497a671a8fa9d76d9d22340140c8b894a86
Min Gyu <71301134+Min0Gyu0Kang@users.noreply.github.com>
2024-02-04 15:01:14 +0900

    programmers lv.1
    그나마 쉬운 것들부터 해본다

---
+++ lv1/oddeven.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int num) {
+    string answer = "";
+    if(num==0){
+        answer="Even";
+    }else if(num%2!=0){
+        answer="Odd";
+    }else{
+        answer="Even";
+    }
+    return answer;
+}
